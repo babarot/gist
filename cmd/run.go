6fb00cfb69769ba5d0b260869a4ac35e430653ab
b4b4r07 <b4b4r07@gmail.com>
2017-06-14 16:48:10 +0900

    Add run command

---
+++ cmd/run.go
@@ -0,0 +1,51 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/b4b4r07/gist/cli"
+	"github.com/spf13/cobra"
+)
+
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "Run the gist snippet as a script",
+	Long:  "Run the gist snippet as a script",
+	RunE:  run,
+}
+
+func run(cmd *cobra.Command, args []string) error {
+	screen, err := cli.NewScreen()
+	if err != nil {
+		return err
+	}
+
+	lines, err := screen.Select()
+	if err != nil {
+		return err
+	}
+
+	for _, line := range lines {
+		fi, err := os.Stat(line.Path)
+		if err != nil {
+			continue
+		}
+		var (
+			origPerm = fi.Mode().Perm()
+			execPerm = os.FileMode(0755).Perm()
+		)
+		if origPerm != execPerm {
+			os.Chmod(line.Path, execPerm)
+			defer os.Chmod(line.Path, origPerm)
+		}
+		if err := cli.Run(line.Path); err != nil {
+			continue
+		}
+	}
+
+	return nil
+}
+
+func init() {
+	RootCmd.AddCommand(runCmd)
+}
