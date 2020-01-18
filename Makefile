bcb6de7f28a23fee4f47c2ffa233b53895984faa
b4b4r07 <b4b4r07@gmail.com>
2020-01-18 22:06:53 +0900

    Migrate from makefile to GH action for release

--- Makefile
+++
@@ -1,3 +0,0 @@
-.PHONY: release
-release:
-	@bash ./release.sh
--- release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# TODO: This will be migrated to GitHub Actions
-bash <(wget -o /dev/null -qO - https://git.io/release-go)
